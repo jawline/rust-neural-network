14f83614d0f2326be3b08e6ae55e7928b6ffe0e7
Blake Loring <blake@parsed.uk>
2018-03-01 18:51:18 +0000

    Implement do_layer

--- src/network.rs
+++ src/network.rs
@@ -1,4 +1,5 @@
 use neuron::Neuron;
+use steps::StepFn;
 
 pub struct Layer {
 	neurons: Vec<Neuron>,
@@ -19,5 +20,21 @@ impl Layer {
 
 	pub fn link(self: &mut Layer, from: usize, to: usize) {
 		self.links.push((from, to));
+	}
+
+	pub fn do_layer<F>(self: &Layer, inputs: Vec<f64>, links: Vec<(usize, usize)>, step: F) -> Vec<f64> 
+	where F: Copy + Fn(&Neuron, f64) -> f64 {
+		let mut results = Vec::new();
+
+		for i in 0..self.neurons.len() {
+			let target_inputs: Vec<f64> = links.iter()
+				.filter(|&&(_, y)| y == i)
+				.map(|&(x, _)| x)
+				.map(|x| inputs[x])
+				.collect();
+			results.push(self.neurons[i].process(target_inputs, step));
+		}
+		
+		results
 	}
 }
--- src/steps.rs
+++ src/steps.rs
@@ -1,3 +1,5 @@
 use neuron::Neuron;
 
+pub type StepFn = Fn(&Neuron, f64) -> f64;
+
 pub const HEAVISIDE : &'static Fn(&Neuron, f64) -> f64 = &|_, v| if v > 0.0 { 1.0 } else { 0.0 };
