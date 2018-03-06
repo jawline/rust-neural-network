43267c521005deb9a73ed8681646625da05c1847
Blake Loring <blake_l@parsed.uk>
2018-03-06 11:43:52 +0000

    Update train.rs

--- src/train.rs
+++ src/train.rs
@@ -38,7 +38,7 @@ pub fn train_network<F, K, L>(p: &mut Network, rounds: usize, factor: f64, train
 	}
 }
 
-pub fn train_network_epoch<F, K, R, L>(p: &mut Network, rounds: usize, items_per_round: usize, factor: f64, next_input: R, classifier: K, step: F, step_deriv: L) 
+pub fn train_network_epoch<F, K, R, L>(p: &mut Network, rounds: usize, factor: f64, training_set: &[Vec<f64>], classifier: K, step: F, step_deriv: L) 
 	where F: Copy + Fn(&Neuron, f64) -> f64,
 		  K: Copy + Fn(f64, f64) -> Vec<f64>,
 		  R: Copy + Fn() -> Vec<f64>,
@@ -46,16 +46,10 @@ pub fn train_network_epoch<F, K, R, L>(p: &mut Network, rounds: usize, items_per
 
 	let learn_rate = factor;
 
-	let training_set: Vec<(Vec<f64>, Vec<f64>)> = (0..items_per_round).map(|_| {
-		let input = next_input();
-		let expected = classifier(input[0], input[1]);
-		(input, expected)
-	}).collect();
-
 	for round in 0..rounds {
 
 		let mut sum_error: f64 = 0.0;
-		let mut epoch_deltas = Vec::new();
+		let mut epoch_delta = Vec::new();
 		
 		for &(ref input, ref expected) in training_set.iter() {
 			let found = p.process(input, step);
@@ -63,10 +57,10 @@ pub fn train_network_epoch<F, K, R, L>(p: &mut Network, rounds: usize, items_per
 			sum_error += delta_found.iter().fold(0.0, |l, n| l + n.abs());
 
 			let deltas = p.backpropogate(delta_found, step_deriv);
-			epoch_deltas.push(deltas);
+			epoch_delta = epoch_deltas.iter().zip(deltas.iter()).map(|(last, next)| last + next);
 		}
 
-		epoch_deltas.iter().for_each(|deltas| p.adjust_weights(deltas, learn_rate));
+		p.adjust_weights(epoch_delta);
 
 		println!("round={} error={} rate={}", round, sum_error, learn_rate);
 	}
