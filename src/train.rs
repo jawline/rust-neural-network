26d8a7e2abf07848ca0f99b02661f8df9a2a33fd
Blake Loring <blake_l@parsed.uk>
2018-03-06 11:46:19 +0000

    Update train.rs

--- src/train.rs
+++ src/train.rs
@@ -13,7 +13,7 @@ pub fn train_perceptron<F, K, R>(p: &mut Neuron, rounds: usize, factor: f64, nex
 	}
 }
 
-pub fn train_network<F, K, L>(p: &mut Network, rounds: usize, factor: f64, training_set: &[Vec<f64>], classifier: K, step: F, step_deriv: L) 
+pub fn train_network<F, K, L>(p: &mut Network, rounds: usize, factor: f64, training_set: &[Vec<f64>], apply_epoch: bool, classifier: K, step: F, step_deriv: L) 
 	where F: Copy + Fn(&Neuron, f64) -> f64,
 		  K: Copy + Fn(f64, f64) -> Vec<f64>,
 		  L: Copy + Fn(f64) -> f64 {
@@ -23,6 +23,7 @@ pub fn train_network<F, K, L>(p: &mut Network, rounds: usize, factor: f64, train
 	for round in 0..rounds {
 
 		let mut sum_error: f64 = 0.0;
+		let mut epoch_delta: Vec<f64> = Vec::new();
 		
 		for input in training_set {
 			let expected = classifier(input[0], input[1]);
@@ -31,36 +32,17 @@ pub fn train_network<F, K, L>(p: &mut Network, rounds: usize, factor: f64, train
 			sum_error += delta_found.iter().fold(0.0, |l, n| l + n.abs());
 
 			let deltas = p.backpropogate(delta_found, step_deriv);
-			p.adjust_weights(&deltas, learn_rate);
+			
+			if !apply_epoch {
+				p.adjust_weights(&deltas, learn_rate);
+			} else {
+				epoch_delta = epoch_deltas.iter().zip(deltas.iter()).map(|(last, next)| last + next);
+			}
 		}
-
-		println!("round={} error={} rate={}", round, sum_error, learn_rate);
-	}
-}
-
-pub fn train_network_epoch<F, K, R, L>(p: &mut Network, rounds: usize, factor: f64, training_set: &[Vec<f64>], classifier: K, step: F, step_deriv: L) 
-	where F: Copy + Fn(&Neuron, f64) -> f64,
-		  K: Copy + Fn(f64, f64) -> Vec<f64>,
-		  R: Copy + Fn() -> Vec<f64>,
-		  L: Copy + Fn(f64) -> f64 {
-
-	let learn_rate = factor;
-
-	for round in 0..rounds {
-
-		let mut sum_error: f64 = 0.0;
-		let mut epoch_delta = Vec::new();
 		
-		for &(ref input, ref expected) in training_set.iter() {
-			let found = p.process(input, step);
-			let delta_found: Vec<f64> = expected.iter().zip(found.iter()).map(|(exp, found)| exp - found).collect();
-			sum_error += delta_found.iter().fold(0.0, |l, n| l + n.abs());
-
-			let deltas = p.backpropogate(delta_found, step_deriv);
-			epoch_delta = epoch_deltas.iter().zip(deltas.iter()).map(|(last, next)| last + next);
+		if apply_epoch {
+			p.adjust_weights(epoch_delta);
 		}
-
-		p.adjust_weights(epoch_delta);
 
 		println!("round={} error={} rate={}", round, sum_error, learn_rate);
 	}
