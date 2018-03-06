cd7ec023a5f43ee8faf5c9f803116dc821db7a8b
Blake Loring <blake_l@parsed.uk>
2018-03-06 12:49:06 +0000

    Update steps.rs

--- src/steps.rs
+++ src/steps.rs
@@ -17,9 +17,3 @@ impl StepFn for Sigmoid {
   fn transfer(n:&Neuron, v:f64) -> f64 { 1.0 / (1.0 + -v.exp()) }
   fn derive(n:&Neuron, v:f64) -> f64 { v * (1.0 - v) }
 }
-
-pub const HEAVISIDE : &'static Fn(&Neuron, f64) -> f64 = &|_, v| if v > 0.0 { 1.0 } else { 0.0 };
-pub const HEAVISIDE_DERIVITIVE : &'static Fn(&Neuron, f64) -> f64 = &|_, v| if v > 0.0 { 1.0 } else { 0.0 };
-
-pub const TRANSFER : &'static Fn(&Neuron, f64) -> f64 = &|_, v| 1.0 / (1.0 + (-v).exp());
-pub const TRANSFER_DERIVITIVE : &'static Fn(f64) -> f64 = &|v| v * (1.0 - v);
