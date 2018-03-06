1c5ce95b4f6f0a4c5fde7fdbde4fcfc36bd0e227
Blake Loring <blake_l@parsed.uk>
2018-03-06 12:47:12 +0000

    Update steps.rs

--- src/steps.rs
+++ src/steps.rs
@@ -1,9 +1,22 @@
 use neuron::Neuron;
 
 pub trait StepFn {
-  fn transfer(&Neuron, f64) -> f64,
-  fn derive(&Neuron, f64) -> f64
+  fn transfer(&Neuron, f64) -> f64;
+  fn derive(&Neuron, f64) -> f64;
 };
+
+pub struct Heaviside {}
+pub struct Sigmoid {}
+
+impl StepFn for Heaviside {
+  fn transfer(n:&Neuron, v:f64) -> f64 { if v > 0.5 { 1.0 } else { 0.0 } }
+  fn derive(n:&Neuron, v:f64) -> f64 { v }
+}
+
+impl StepFn for Sigmoid {
+  fn transfer(n:&Neuron, v:f64) -> f64 { 1.0 / (1.0 + -v.exp()) }
+  fn derive(n:&Neuron, v:f64) -> f64 { v * (1.0 - v) }
+}
 
 pub const HEAVISIDE : &'static Fn(&Neuron, f64) -> f64 = &|_, v| if v > 0.0 { 1.0 } else { 0.0 };
 pub const HEAVISIDE_DERIVITIVE : &'static Fn(&Neuron, f64) -> f64 = &|_, v| if v > 0.0 { 1.0 } else { 0.0 };
